df266c7b4b610f42a443ddf03592c013a0e25274
Peshk0 <petar.tonev.pp25@pgmtplovdiv.info>
2026-02-11 10:07:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp76
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+         {
+            Console.OutputEncoding = 
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Въведете десетично число: ");
+            int n = int.Parse(Console.ReadLine());
+            string binary = "";
+            while (n > 0)
+            {
+                binary = (n % 2) + binary;
+                n /= 2;
+            }
+         }   Console.WriteLine($"Двоично: Камбуракис ала КМК {binary}");
+    }
+}
